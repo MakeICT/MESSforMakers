2c1432b12b7c959b014b9fb7ae86be7705898b4b
skippotter <skippotter1@gmail.com>
2017-09-17 06:09:37 +0000

    Updated README Added LICENSE and TODO list
    Started a code skeleton the runs and builds, shows basic framework for controllers and views

---
+++ main.go
@@ -0,0 +1,56 @@
+/*
+ Domesticated Apricot - An open source member and event management platform
+    Copyright (C) 2017  Sam Schurter
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+package main
+
+import (
+	"fmt"
+	"github.com/gorilla/mux"
+	"github.com/justinas/alice"
+	"github.com/makeict/Domesticated-Apricot/controllers"
+	"log"
+	"net/http"
+	"time"
+)
+
+type App struct {
+	*mux.Router
+}
+
+func main() {
+	app := &App{mux.NewRouter()}
+	commonHandlers := alice.New(loggingHandler)
+	userC := controllers.User
+	app.Handle("/", commonHandlers.ThenFunc(RootHandler))
+	app.Handle("/user", commonHandlers.ThenFunc(userC.Index))
+	log.Fatal(http.ListenAndServe(":8080", app))
+}
+
+//middleware
+func loggingHandler(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		t1 := time.Now()
+		h.ServeHTTP(w, r)
+		t2 := time.Now()
+		fmt.Printf("[%s] %q %v\n", r.Method, r.URL.String(), t2.Sub(t1))
+	})
+}
+
+//obsolete, will all be handled in controllers
+func RootHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "you got the root handler")
+}
---
+++ views/userV.go
@@ -0,0 +1,51 @@
+/*
+ Domesticated Apricot - An open source member and event management platform
+    Copyright (C) 2017  Sam Schurter
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+package views
+
+import (
+	"html/template"
+	"log"
+	"path/filepath"
+)
+
+type UserView struct {
+	View
+	//add custom pages for a controller here
+}
+
+var User UserView
+
+func UserFiles() []string {
+	files, err := filepath.Glob("templates/user/includes/*.gohtml")
+	if err != nil {
+		log.Panic(err)
+	}
+	files = append(files, LayoutFiles()...)
+	return files
+}
+
+func init() {
+	indexFiles := append(UserFiles(), "templates/user/index.gohtml")
+	User.Index = Page{
+		Template: template.Must(template.New("index").ParseFiles(indexFiles...)),
+		Layout:   "index",
+	}
+
+	//parse other needed templates here, for each page
+
+}
