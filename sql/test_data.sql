e62534981231659d88279fa2df1a0c9c3ce19a32
Sam Schurter <skippotter1@gmail.com>
2017-11-17 20:58:12 -0600

    Schema (#55)
    * Adding some basic work on creating a schema
    
    This is just some ideas I have had based on the current Requirement
    documents. Not complete and missing lots of things, like all of the
    event tables.
    
    * Adding hacky db reload script
    
    * Cleaning up rbac tables
    
    I renamed the permission_set tables to the role, which is what they
    were.
    
    * Starting to add event tables.
    
    * Some cleanup of the equipment tables.
    
    * Just a test commit.
    
    * update readme
    
    * creates first-draft database schema
    
    * updates the readme to upgrade postgres, tweaks the reload script to include adding some test data

---
+++ sql/test_data.sql
@@ -0,0 +1,14 @@
+-- Some example data to populate a database
+-- This should only be run from the reload script which drops and creates the database fresh
+
+-- add this to the database with the following:
+-- psql <connection string> -f test_tables.sql
+
+INSERT INTO rbac_role (name) VALUES ('DEFAULT');
+
+INSERT INTO member (name, username, password, dob, phone, membership_status_id, rbac_role_id) VALUES 
+('Name One', 'email1@address.com', 'aaaaaaaa', '1970-1-1', '316-555-1234', 1, 1),
+('Name Two', 'email2@address.com', 'aaaaaaaa', '1970-1-1', '316-555-1234', 1, 1),
+('John Doe', 'email3@address.com', 'aaaaaaaa', '1970-1-1', '316-555-1234', 1, 1),
+('Jane Doe', 'email4@address.com', 'aaaaaaaa', '1970-1-1', '316-555-1234', 1, 1);
+
