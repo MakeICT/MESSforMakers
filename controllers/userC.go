c783ea1cc618df1102510d70cf77859a867aa4f2
skippotter <skippotter1@gmail.com>
2018-02-15 19:17:20 +0000

    adds basic form parsing

--- controllers/userC.go
+++ controllers/userC.go
@@ -20,6 +20,7 @@ package controllers
 import (
 	"net/http"
 
+	"github.com/gorilla/schema"
 	"github.com/jmoiron/sqlx"
 
 	"github.com/makeict/MESSforMakers/models"
@@ -32,12 +33,48 @@ import (
 // the struct defines what the controller needs to be able to pass into any given page it needs to render
 type UserController struct {
 	Controller
-	DB *sqlx.DB
+	DB      *sqlx.DB
+	Decoder *schema.Decoder
 }
 
 // setup function that stores the database pool in the controller, or other things if necessary
 func User(db *sqlx.DB) UserController {
-	return UserController{DB: db}
+	//TODO make a map for holding state names and abbreviations, prepopulate it at setup time with a list read from file.
+	// list available here: https://gist.github.com/mshafrir/2646763
+	return UserController{DB: db, Decoder: schema.NewDecoder()}
+}
+
+type Address struct {
+	Line1 string
+	Line2 string
+	City  string
+	State string
+	Zip   string
+}
+
+type Ice struct {
+	Name     string `schema:"name"`
+	Phone    string `schema:"phone"`
+	Relation string `schema:"relation"`
+}
+
+type Person struct {
+	FirstName string `schema:"firstname"`
+	LastName  string `schema:"lastname"`
+	Address   Address
+	Phone     string `schema:"phone"`
+	OfAge     bool   `schema:"ofage"`
+	Guardian  string `schema:"guardian"`
+	Email     string `schema:"email"`
+	Password  string `schema:"-"`
+	Ice       Ice    `schema:"ice"`
+}
+
+type Login struct {
+	Email         string `schema:"email"`
+	EmailCheck    string `schema:"emailcheck"`
+	Password      string `schema:"password"`
+	PasswordCheck string `schema:"passwordcheck"`
 }
 
 // a handler
@@ -60,18 +97,43 @@ func (c *UserController) Create() func(w http.ResponseWriter, r *http.Request) {
 
 		if r.Method == "GET" {
 
-			body := ""
+			person := new(Person)
+			body := struct{ Person *Person }{Person: person}
+			// body := ""
 			if err := views.User.New.Render(w, body); err != nil {
 				http.Error(w, err.Error(), http.StatusInternalServerError)
 			}
 
 		} else if r.Method == "POST" {
-			body := "Arrived via post request. Not set up to save in DB yet"
-			// need a User.Validate() function to check a populated User object for correctness, that would be a database function.
-			// the User object should be passed around, so if there are errors, it can be reused to populate the form again
-			if err := views.ErrorPage.Index.Render(w, body); err != nil {
+
+			person, _ := parseSignupForm(r, c.Decoder)
+			//TODO if email submitted is not valid, or does not match emailcheck, it should not be returned to fill form
+			//TODO if password submitted is not valid, or does not match passwordcheck, it should not be returned to fill form
+			body := struct{ Person *Person }{Person: person}
+
+			//TODO need a Person.Validate() function to check a populated User object for correctness, that would be a database function? The Person object itself should probably be defined in the model, not here.
+			// the Person object should be passed around, so if there are errors, it can be reused to populate the form again
+			//TODO how to communicate errors
+			if err := views.User.New.Render(w, body); err != nil {
 				http.Error(w, err.Error(), http.StatusInternalServerError)
 			}
 		}
 	}
+}
+
+func parseSignupForm(r *http.Request, d *schema.Decoder) (*Person, *Login) {
+	if err := r.ParseForm(); err != nil {
+		//TODO handle this error
+	}
+
+	p := new(Person)
+	l := new(Login)
+	if err := d.Decode(p, r.PostForm); err != nil {
+		//TODO handle this error
+	}
+	if err := d.Decode(l, r.PostForm); err != nil {
+		//TODO handle this error
+	}
+
+	return p, l
 }
