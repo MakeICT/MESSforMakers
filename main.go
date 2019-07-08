a35b8d4fc01784568e5d28ea8740718f205f84ba
Sam Schurter <skippotter1@gmail.com>
2019-07-08 15:34:53 -0500

    initial work

--- main.go
+++ main.go
@@ -27,14 +27,20 @@ import (
 	"github.com/makeict/MESSforMakers/util"
 )
 
-const appPort = "8080"
-
+//Main reads the configuration immediately and dies if it can't be read.
+//There is no default configuration and no command line flags.
+//All options are contained in the config.json file
 func main() {
+	//Read the configuration and die if it can't be read.
+	//This does NOT guarantee that sensible options have been set, only that the file can be read.
 	config, err := InitConfig("config.json")
 	if err != nil {
+		//No log files have been set up yet, so just dump the error to stdout
 		fmt.Print("Cannot parse the configuration file")
 		panic(1)
 	}
+
+	//TODO newApplication should return an error if the configuration is invalid
 	app := newApplication(config)
 	defer app.logger.Close()
 
